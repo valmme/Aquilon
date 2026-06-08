3a21cd5dd0ce15b74f948d4dde68e38ebf7e6d80
valmme <vagif.fb12@gmail.com>
2026-06-08 20:40:38 +0400

    dropped_item.h

---
+++ include/inv/dropped_item.h
@@ -0,0 +1,25 @@
+#ifndef AQUILON_DROPPED_ITEM_H
+#define AQUILON_DROPPED_ITEM_H
+
+#include <SDL3/SDL.h>
+#include <vector>
+#include "item.h"
+#include "player.h"
+
+struct DroppedItem {
+    Item* item = nullptr;
+    float x = 0.0f;
+    float y = 0.0f;
+};
+
+class DroppedItemSystem {
+public:
+    void spawn(Item* item, float world_x, float world_y);
+    void draw(SDL_Renderer* renderer, const Camera& cam) const;
+    std::vector<Item*> pickup_near(float world_x, float world_y, float radius = 48.0f);
+
+private:
+    std::vector<DroppedItem> dropped;
+};
+
+#endif // AQUILON_DROPPED_ITEM_H
